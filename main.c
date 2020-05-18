9aa02ba5454b7bee6f4bc47fd92edf43546c3dd1
PanAlle <217328@studenti.unimore.it>
2020-05-18 12:50:06 +0200

    Mod Now the wrtie to file works pretty good

--- main.c
+++ main.c
@@ -7,33 +7,32 @@
 typedef struct {
     char *FirstName;
     char *LastName;
-    int  PointOfAssignement[4];
+    int PointOfAssignement[4];
     char *EmailAddress;
-}Student;
+} Student;
 
 
 void ReadFile(Student *Student) {
     FILE *fp;
     fp = fopen("C:\\Users\\utente\\CLionProjects\\Grading_s3g1\\Student_list.txt", "r");
     int i = 0;
-    if (fp  == NULL)
-    {
+    if (fp == NULL) {
         printf("Can't find the file");
     }
-    char *buf = (char*)malloc(256);
+    char *buf = (char *) malloc(256);
     char *tmp;
 
-    while (fgets(buf, 255, fp) != NULL){
+    while (fgets(buf, 255, fp) != NULL) {
         //First strtok require tyo pass the string to be reduced in token
         tmp = strtok(buf, ",");
         Student[i].FirstName = strdup(tmp);
-        //strcpy(Student[i].FirstName, tmp);
 
         //Following call of strtok require NULL as argument, that say to the string to continue tokenize the string previously passed
         tmp = strtok(NULL, ",");
         Student[i].LastName = strdup(tmp);
-        //Student[i].LastName = malloc(sizeof(tmp)+1);
-        //strcpy(Student[i].LastName, tmp);
+        //Remove the first charcter since it is a space
+        memmove(Student[i].LastName, Student[i].LastName + 1, strlen(Student[i].LastName));
+
 
         for (int j = 0; j < NUMBER_OF_ASSIGNMENT; ++j) {
             tmp = strtok(NULL, ",");
@@ -42,8 +41,11 @@ void ReadFile(Student *Student) {
 
         tmp = strtok(NULL, ",");
         Student[i].EmailAddress = strdup(tmp);
-        //Delete the last value, that is a /n new line command
-        Student[i].EmailAddress[strlen(Student[i].EmailAddress) - 1] = 0;
+        memmove(Student[i].EmailAddress, Student[i].EmailAddress + 1, strlen(Student[i].EmailAddress));
+        //Delete the last value, that is a /n new line command. This apply to all line but the last, that's why we need a if statement
+        if (Student[i].EmailAddress[strlen(Student[i].EmailAddress) - 1] == '\n') {
+            Student[i].EmailAddress[strlen(Student[i].EmailAddress) - 1] = 0;
+        }
         //Iterate throught the struct
         i++;
     }
@@ -53,16 +55,15 @@ void ReadFile(Student *Student) {
 }
 
 
-void WriteFile(Student *Student){
+void WriteFile(Student *Student) {
     FILE *fw;
     fw = fopen("C:\\Users\\utente\\CLionProjects\\Grading_s3g1\\Destination_list.txt", "w");
-    if (fw  == NULL)
-    {
+    if (fw == NULL) {
         printf("Can't find the file");
     }
     int i = 0;
     float Points = 0;
-    for (int j = 0; j < NUMBER_OF_ASSIGNMENT; j++){
+    for (int j = 0; j < NUMBER_OF_ASSIGNMENT; j++) {
         Points += Student[i].PointOfAssignement[j];
     }
     float Grade = 0;
@@ -77,13 +78,11 @@ void WriteFile(Student *Student){
     else if (Points >= 86 && Points <= 91) Grade = 1.7;
     else if (Points >= 91 && Points <= 96) Grade = 1.3;
     else if (Points >= 96 && Points <= 100) Grade = 1.0;
-    //printf("Counter %d", counter);
-    //for (int j = 0; j < 2; j++){
-    for (int k = 0; k < 2 ; ++k) {
-    fprintf(fw, "%s%s<%s>:%1.1f\n", Student[i].FirstName, Student[i].LastName, Student[i].EmailAddress, Grade);
-    i++;
+
+    for (int k = 0; k < 2; ++k) {
+        fprintf(fw, "%s %s <%s>: %1.1f\n", Student[i].FirstName, Student[i].LastName, Student[i].EmailAddress, Grade);
+        i++;
     }
-    //}
     fclose(fw);
 
 }
@@ -91,9 +90,8 @@ void WriteFile(Student *Student){
 int main() {
     Student Student[100];
     ReadFile(Student);
-    printf("Counter in main loop %d\n", sizeof(Student)/sizeof(Student[0]) );
+    printf("Counter in main loop %d\n", sizeof(Student) / sizeof(Student[0]));
     WriteFile(Student);
-
 
 
 }
