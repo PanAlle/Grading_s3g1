0c7291a7493d7fc7753f0830c30048905deb7e85
PanAlle <217328@studenti.unimore.it>
2020-05-18 14:32:52 +0200

    Finished the program works. Need to insert a function that ask for file path in order to make it more portable

--- main.c
+++ main.c
@@ -9,10 +9,11 @@ typedef struct {
     char *LastName;
     int PointOfAssignement[4];
     char *EmailAddress;
+    int counter;
 } Student;
 
 
-void ReadFile(Student *Student) {
+void ReadFile(Student *Student, int *counter) {
     FILE *fp;
     fp = fopen("C:\\Users\\utente\\CLionProjects\\Grading_s3g1\\Student_list.txt", "r");
     int i = 0;
@@ -47,6 +48,7 @@ void ReadFile(Student *Student) {
             Student[i].EmailAddress[strlen(Student[i].EmailAddress) - 1] = 0;
         }
         //Iterate throught the struct
+        *counter = i;
         i++;
     }
 
@@ -55,16 +57,17 @@ void ReadFile(Student *Student) {
 }
 
 
-void WriteFile(Student *Student) {
+void WriteFile(Student *Student, int* counter) {
     FILE *fw;
     fw = fopen("C:\\Users\\utente\\CLionProjects\\Grading_s3g1\\Destination_list.txt", "w");
     if (fw == NULL) {
         printf("Can't find the file");
     }
-    int i = 0;
+
+    for (int k = 0; k <= *counter; ++k) {
     float Points = 0;
     for (int j = 0; j < NUMBER_OF_ASSIGNMENT; j++) {
-        Points += Student[i].PointOfAssignement[j];
+        Points += Student[k].PointOfAssignement[j];
     }
     float Grade = 0;
     if (Points >= 0.0 && Points <= 50) Grade = 5.0;
@@ -79,19 +82,50 @@ void WriteFile(Student *Student) {
     else if (Points >= 91 && Points <= 96) Grade = 1.3;
     else if (Points >= 96 && Points <= 100) Grade = 1.0;
 
-    for (int k = 0; k < 2; ++k) {
-        fprintf(fw, "%s %s <%s>: %1.1f\n", Student[i].FirstName, Student[i].LastName, Student[i].EmailAddress, Grade);
-        i++;
+
+        fprintf(fw, "%s %s <%s>: %1.1f\n", Student[k].FirstName, Student[k].LastName, Student[k].EmailAddress, Grade);
     }
     fclose(fw);
 
 }
 
+void HighScore(Student *Student, int* counter){
+    float high_score = 0;
+    float max_grade = 0;
+    int  ID = 0;
+
+    for (int k = 0; k <= *counter; ++k) {
+        float current_score = 0;
+    for (int i = 0; i < NUMBER_OF_ASSIGNMENT; ++i) {
+        current_score += Student[k].PointOfAssignement[i];
+    }
+    if (current_score > high_score){
+        high_score = current_score;
+        ID = k;
+    }
+        printf("High score %f", high_score);
+        if (high_score >= 0.0 && high_score <= 50) max_grade = 5.0;
+        else if (high_score >= 50 && high_score <= 55.5) max_grade = 4.0;
+        else if (high_score >= 55.5 && high_score <= 60.5) max_grade = 3.7;
+        else if (high_score >= 60.5 && high_score <= 65.5) max_grade = 3.3;
+        else if (high_score >= 65.5 && high_score <= 70.5) max_grade = 3.0;
+        else if (high_score >= 70.5 && high_score <= 75.5) max_grade = 2.7;
+        else if (high_score >= 75.5 && high_score <= 81) max_grade = 2.3;
+        else if (high_score >= 81 && high_score <= 86) max_grade = 2.0;
+        else if (high_score >= 86 && high_score <= 91) max_grade = 1.7;
+        else if (high_score >= 91 && high_score <= 96) max_grade = 1.3;
+        else if (high_score >= 96 && high_score <= 100) max_grade = 1.0;
+    }
+    printf("%s %s <%s>: %1.1f\n", Student[ID].FirstName, Student[ID].LastName, Student[ID].EmailAddress, max_grade);
+
+}
+
 int main() {
+    int counter = 0;
     Student Student[100];
-    ReadFile(Student);
-    printf("Counter in main loop %d\n", sizeof(Student) / sizeof(Student[0]));
-    WriteFile(Student);
+    ReadFile(Student,  &counter);
+    WriteFile(Student, &counter);
+    HighScore(Student, &counter);
 
 
 }
