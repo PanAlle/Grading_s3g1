973d2180ec506a5095c601b7346e46f27f10696a
PanAlle <217328@studenti.unimore.it>
2020-05-25 11:55:27 +0200

    Insert comments The code is now commented and cleaned of some misspelled words.

--- main.c
+++ main.c
@@ -3,54 +3,59 @@
 #include <string.h>
 
 #define NUMBER_OF_ASSIGNMENT 4
-
+//typedef the struct
 typedef struct {
     char *FirstName;
     char *LastName;
-    int PointOfAssignement[4];
+    int PointOfAssignment[4];
     char *EmailAddress;
 } Student;
 
 
 void ReadFile(Student *Student, int *counter, char *ReadFileName) {
+    //Create a file pointer
     FILE *fp;
+    //Assign to the file pointer the file address, in read mode
     fp = fopen( ReadFileName, "r");
     int i = 0;
     if (fp == NULL) {
         printf("Can't find the file");
     }
+    //create a char buffer that can store line
     char *buf = (char *) malloc(256);
+    //cgreat a char pinter to save temporary information
     char *tmp;
 
     while (fgets(buf, 255, fp) != NULL) {
-        //First strtok require tyo pass the string to be reduced in token
+        //First strtok require tyo pass the string to be reduced in token, strtok automatically allocate memory for the tmp pointer
         tmp = strtok(buf, ",");
         Student[i].FirstName = strdup(tmp);
 
         //Following call of strtok require NULL as argument, that say to the string to continue tokenize the string previously passed
         tmp = strtok(NULL, ",");
         Student[i].LastName = strdup(tmp);
-        //Remove the first charcter since it is a space
+        //Remove the first charcter after the comma, since it is a space
         memmove(Student[i].LastName, Student[i].LastName + 1, strlen(Student[i].LastName));
 
-
+        //Read the points iteratively into a int array
         for (int j = 0; j < NUMBER_OF_ASSIGNMENT; ++j) {
             tmp = strtok(NULL, ",");
-            Student[i].PointOfAssignement[j] = atoi(tmp);
+            Student[i].PointOfAssignment[j] = atoi(tmp);
         }
 
         tmp = strtok(NULL, ",");
         Student[i].EmailAddress = strdup(tmp);
         memmove(Student[i].EmailAddress, Student[i].EmailAddress + 1, strlen(Student[i].EmailAddress));
-        //Delete the last value, that is a /n new line command. This apply to all line but the last, that's why we need a if statement
+        //Delete the last value, that is a "/n" new line command. This apply to all line but the last, that's why we need a if statement
         if (Student[i].EmailAddress[strlen(Student[i].EmailAddress) - 1] == '\n') {
             Student[i].EmailAddress[strlen(Student[i].EmailAddress) - 1] = 0;
         }
-        //Iterate throught the struct
+        //Keep a counter of the reading, in order to use it during printing
         *counter = i;
+        //Iterate throught the struct
         i++;
     }
-
+        //close the file, free pointer
     fclose(fp);
 
 }
@@ -62,12 +67,14 @@ void WriteFile(Student *Student, int* counter, char* WriteFileName) {
     if (fw == NULL) {
         printf("Can't find the file");
     }
-
+    //iterate throught the instances saved in the Student Struct
     for (int k = 0; k <= *counter; ++k) {
     float Points = 0;
+    //Calculate the points of the assignment
     for (int j = 0; j < NUMBER_OF_ASSIGNMENT; j++) {
-        Points += Student[k].PointOfAssignement[j];
+        Points += Student[k].PointOfAssignment[j];
     }
+    //compute the grades
     float Grade = 0;
     if (Points >= 0.0 && Points <= 50) Grade = 5.0;
     else if (Points >= 50 && Points <= 55.5) Grade = 4.0;
@@ -80,7 +87,7 @@ void WriteFile(Student *Student, int* counter, char* WriteFileName) {
     else if (Points >= 86 && Points <= 91) Grade = 1.7;
     else if (Points >= 91 && Points <= 96) Grade = 1.3;
     else if (Points >= 96 && Points <= 100) Grade = 1.0;
-
+    //print to file
     fprintf(fw, "%s %s <%s>: %1.1f\n", Student[k].FirstName, Student[k].LastName, Student[k].EmailAddress, Grade);
 
     }
@@ -96,7 +103,7 @@ void HighScore(Student *Student, int* counter){
     for (int k = 0; k <= *counter; ++k) {
         float current_score = 0;
     for (int i = 0; i < NUMBER_OF_ASSIGNMENT; ++i) {
-        current_score += Student[k].PointOfAssignement[i];
+        current_score += Student[k].PointOfAssignment[i];
     }
     if (current_score > high_score){
         high_score = current_score;
@@ -124,6 +131,7 @@ int main() {
     char *WriteFileName = "C:\\Users\\utente\\CLionProjects\\Grading_s3g1\\Destination_list.txt";
     //Initialize a counter for the number of Student instaces are present in the Read File
     int counter = 0;
+    //Create a 100 instances array of Student strcutures
     Student Student[100];
     ReadFile(Student,  &counter, ReadFileName);
     WriteFile(Student, &counter, WriteFileName);
